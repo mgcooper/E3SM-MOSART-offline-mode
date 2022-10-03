4b16dba82f87a296a829fd978d40ef7f4d2400ee
mgcooper <mguycooper@gmail.com>
2022-10-03 16:16:48 -0700

    added more options to cp_compy2local

--- sh/cp_compy2local.sh
+++ sh/cp_compy2local.sh
@@ -1,33 +1,58 @@
 #!/bin/bash
 
+# ------------------------------------------------------------
 # to copy an individual file to this directory:
+# ------------------------------------------------------------
 # SRCNAME=dlnd.streams.txt.lnd.gpcc.icom_mpas
 # SRCPATH=$COMPYDATAPATH/usrdat/$SRCNAME
 # DSTPATH=$(pwd)/$SRCNAME
-
 # rsync -a -e ssh -P "$SRCPATH" "$DSTPATH"
 
+# ------------------------------------------------------------
 # to copy the hexwatershed mesh file:
+# ------------------------------------------------------------
 # SRCNAME=hexwatershed.json
 # SRCPATH=coop558@compy01:/compyfs/liao313/04model/pyhexwatershed/susquehanna/pyhexwatershed20220901014/hexwatershed/$SRCNAME
 # DSTPATH=$(pwd)/$SRCNAME
+# rsync -a -e ssh -P "$SRCPATH" "$DSTPATH"
 
+# ------------------------------------------------------------
 # to copy the flowline:
+# ------------------------------------------------------------
 # SRCNAME=flowline_conceptual.geojson
 # SRCPATH=coop558@compy01:/compyfs/liao313/04model/pyhexwatershed/susquehanna/pyhexwatershed20220901014/pyflowline/0001/$SRCNAME
 # DSTPATH=$(pwd)/$SRCNAME
-
 # rsync -a -e ssh -P "$SRCPATH" "$DSTPATH"
 
-
+# ------------------------------------------------------------
 # to copy a list of files:
-SRCLIST=("flowline_conceptual.geojson" "flowline_conceptual_info.json")
-SRCPATH=coop558@compy01:/compyfs/liao313/04model/pyhexwatershed/susquehanna/pyhexwatershed20220901014/pyflowline/0001
-DSTPATH=/Users/coop558/mydata/icom/hexwatershed/pyhexwatershed20220901014/pyflowline
+# ------------------------------------------------------------
+# SRCLIST=("flowline_conceptual.geojson" "flowline_conceptual_info.json")
+# SRCPATH=coop558@compy01:/compyfs/liao313/04model/pyhexwatershed/susquehanna/pyhexwatershed20220901014/pyflowline/0001
+# DSTPATH=/Users/coop558/mydata/icom/hexwatershed/pyhexwatershed20220901014/pyflowline
+# for SRCNAME in "${SRCLIST[@]}"; do
+#     rsync -a -e ssh -P "$SRCPATH/$SRCNAME" "$DSTPATH/$SRCNAME"    
+#     # printf "%s/%s\n" "$SRCPATH" "$SRCNAME"
+#     # printf "%s/%s\n" "$DSTPATH" "$SRCNAME"
+# done
 
+SRCLIST=("mpas.geojson" "mpas_mesh_info.json")
+SRCPATH=coop558@compy01:/compyfs/liao313/04model/pyhexwatershed/susquehanna/pyhexwatershed20220901014/pyflowline
+DSTPATH=/Users/coop558/mydata/icom/hexwatershed/pyhexwatershed20220901014/pyflowline
 for SRCNAME in "${SRCLIST[@]}"; do
-    rsync -a -e ssh -P "$SRCPATH/$SRCNAME" "$DSTPATH/$SRCNAME"
-    
+    rsync -a -e ssh -P "$SRCPATH/$SRCNAME" "$DSTPATH/$SRCNAME"    
     # printf "%s/%s\n" "$SRCPATH" "$SRCNAME"
     # printf "%s/%s\n" "$DSTPATH" "$SRCNAME"
 done
+
+# ------------------------------------------------------------
+# to copy a directory
+# ------------------------------------------------------------
+# SRCPATH=coop558@compy01:/compyfs/liao313/04model/pyhexwatershed/susquehanna/pyhexwatershed20220901014/pyflowline/0001
+# DSTPATH=/Users/coop558/mydata/icom/hexwatershed/pyhexwatershed20220901014/pyflowline
+
+# this will create the directory at the end of SRCPATH in DSTPATH
+# rsync -a -e ssh -P "$SRCPATH" "$DSTPATH"
+
+# this will not create the directory at the end of SRCPATH in DSTPATH
+# rsync -a -e ssh -P "$SRCPATH/" "$DSTPATH"
