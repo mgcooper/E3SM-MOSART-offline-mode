10b102af8fe6bd9ed6e7434b84d5d0c05e115010
mgcooper <mguycooper@gmail.com>
2022-10-03 16:17:16 -0700

    first working version of makeDamDependency function

--- functions/mosart/makeDamDependency/makeDamDependency.m
+++ functions/mosart/makeDamDependency/makeDamDependency.m
@@ -1,7 +1,7 @@
-function Dams = makeDamDependency(Dams,Mesh,varargin)
-%MAKEDAMDEPENDENCY adds an array of logical indices called 'DependentCells' to
-%input table Dams that are true for mesh cells in Mesh that 'depend' on each
-%dam in Dams.
+function Dams = makeDamDependency(Dams,Mesh,Line,varargin)
+%makeDamDependency adds an array of logical indices called 'DependentCells' to
+%input table Dams. The DependentCells for each dam are true for mesh cells in Mesh that
+%'depend' on each dam in Dams.
 % 
 % Syntax:
 % 
@@ -28,19 +28,23 @@ p                 = inputParser;
 p.FunctionName    = 'makeDamDependency';
 p.StructExpand    = false;
    
-addRequired(p,    'Dams',                  @(x)istable(x)            );
-addRequired(p,    'Mesh',                  @(x)isstruct(x)           );
-addParameter(p,   'searchradius', 10000,   @(x)isnumeric(x)          );
-addParameter(p,   'plotfig',      false,   @(x)islogical(x)          );
-addParameter(p,   'damname',      'all',   @(x)ischar(x)|isstring(x) );
+addRequired(p,    'Dams',                 @(x)istable(x)            );
+addRequired(p,    'Mesh',                 @(x)isstruct(x)           );
+addRequired(p,    'Line',                 @(x)isstruct(x)           );
+addParameter(p,   'searchradius', 10000,  @(x)isnumeric(x)          );
+addParameter(p,   'plotfig',      false,  @(x)islogical(x)          );
+addParameter(p,   'damname',      'all',  @(x)ischar(x)|isstring(x) );
 
-parse(p,Dams,Mesh,varargin{:});
+parse(p,Dams,Mesh,Line,varargin{:});
    
 plotfig  = p.Results.plotfig;
 damname  = p.Results.damname;
 rxy      = p.Results.searchradius;
    
 %------------------------------------------------------------------------------
+
+% NOTE: lat/lon/x/yline and cap are not used but keep them if we want to use
+% the flowline rather than the mesh cells that contain the flowline. 
 
 % if a single dam was requested, subset Dams
 if damname ~= "all"
@@ -51,28 +55,56 @@ end
 cap      = Dams.Capacity_km3;
 
 % get the x,y location of all the mesh cell centroids
-clat     = [Mesh.dLatitude_center_degree];   clat = clat(:);
-clon     = [Mesh.dLongitude_center_degree];  clon = clon(:);
-celev    = [Mesh.Elevation];                 celev = celev(:);
+latmesh  = [Mesh.dLatitude_center_degree];   latmesh = latmesh(:);
+lonmesh  = [Mesh.dLongitude_center_degree];  lonmesh = lonmesh(:);
+zmesh    = [Mesh.Elevation];                 zmesh = zmesh(:);
+
+% get the x,y location of the flowlines. imesh is the indices of the hex cells
+% that contain a flowline indici
+latline     = [];
+lonline     = [];
+imeshline   = [];
+for n = 1:numel(Line)
+   latline     = [latline;nan;Line(n).Lat];
+   lonline     = [lonline;nan;Line(n).Lon];
+   imeshline   = [imeshline;Line(n).iMesh];
+end
 
 % project to utm. i used this to find the zone: utmzone(clat(1),clon(1))
-proj     = projcrs(32618,'Authority','EPSG');
-[cx,cy]  = projfwd(proj,clat,clon);
+proj           = projcrs(32618,'Authority','EPSG');
+[xmesh,ymesh]  = projfwd(proj,latmesh,lonmesh);          % mesh
+[xline,yline]  = projfwd(proj,latline,lonline);          % flowline
+[xdams,ydams]  = projfwd(proj,Dams.Lat,Dams.Lon);        % dams
 
-% get the x,y location of the dams
-[dx,dy]  = projfwd(proj,Dams.Lat,Dams.Lon);
+% create a boundary around the mesh and exclude dams outside the boundary
+ibounds     = boundary(xmesh,ymesh);
+mbounds     = polyshape(xmesh(ibounds),ymesh(ibounds));
+inbounds    = inpolygon(xdams,ydams,mbounds.Vertices(:,1),mbounds.Vertices(:,2));
+xdams       = xdams(inbounds);
+ydams       = ydams(inbounds);
+cap         = cap(inbounds);
+numdams     = sum(inbounds);
 
-% exclude dams outside the mesh
-bound    = boundary(cx,cy);
-bound    = polyshape(cx(bound),cy(bound));
-inbound  = inpolygon(dx,dy,bound.Vertices(:,1),bound.Vertices(:,2));
-dx       = dx(inbound);
-dy       = dy(inbound);
-cap      = cap(inbound);
-numdams  = sum(inbound);
+% subset the Dams table
+Dams = Dams(inbounds,:);
 
-% find the hex cell nearest each dam
-[idx,~]  = dsearchn([cx cy],[dx dy]);
+% keep this to find all hex cells that contain a flowline using the Mesh
+% attribute iSegment, which should work with updated hexwatershed output
+% unique([Mesh.iSegment])
+
+% build the kdtree for the mesh
+MeshTree    = createns([xmesh ymesh]);
+
+% find the hex cell nearest each dam that contains a flowline
+MeshLine    = Mesh(imeshline);
+
+% i think it is safe to overwrite cx/cy here 
+xmeshline   = xmesh(imeshline);      % mesh-flowline x
+ymeshline   = ymesh(imeshline);      % mesh-flowline y
+zmeshline   = zmesh(imeshline);   % mesh-flowline elevation
+
+% imeshdams are the indices of the mesh cells nearest each dam
+[imeshdams,~]  = dsearchn([xmeshline ymeshline],[xdams ydams]);
 
 % this will hold the dependent indices for each dam:
 idepends = cell(numdams,1);
@@ -80,95 +112,86 @@ idepends = cell(numdams,1);
 % plot the hex centroids, the dams, and the starting points. durign the
 % loop the dependent cells can be plotted on top of this map
 if plotfig == true
-   macfig; 
-   scatter(cx,cy,'filled'); hold on;
-   scatter(dx,dy,cap.*1000,'filled');
-   scatter(cx(idx),cy(idx),100,'g');
+   
+   % green = mesh cells along the flowline. magenta = mesh cells nearest each
+   % dam along the flowline. black = flowline
+   figure('Position', [50 60 1200 1200]); hold on; 
+   patch_hexmesh(Mesh); % use 'FaceMapping','Elevation' to see the elevation
+   patch_hexmesh(Mesh(imeshline),'FaceColor','g');
+   patch_hexmesh(MeshLine(imeshdams),'FaceColor','m'); hold on;
+   geoshow(Line); scatter(Dams.Lon(inbounds),Dams.Lat(inbounds),'b','filled');
+   
+   % use the cartesian coords:
+   %    macfig; scatter(mx,my,'filled'); hold on;
+   %    scatter(dx,dy,cap.*1000,'filled');
+   %    plot(fx,fy,'m');
+   %    scatter(mx(idx),my(idx),100,'g','filled');
 end
-
 
 % for each dam, start at the nearest cell and find all cells with lower
 % elevation, then find all cells within rXY distance from each cell
 for n = 1:numdams
 
-   % the x,y of the cell nearest this dam
-   xn = cx(idx(n));
-   yn = cy(idx(n));
+   % the elevation of the cell nearest this dam is used as a threshold to
+   % exclude cells above the dam. this could be replaced by the elevation of
+   % the dam, or the elevation of the cell that contains the dam.
+   elev  = zmeshline(imeshdams(n));
+   
+   % gather the mesh cells that are below this dam along the flowline. these
+   % are the query points for the candidate tree, to find all points in the
+   % candidate tree within rxy distance of these points.   
+   iquery   = zmeshline<elev;
+   xyquery  = [xmeshline(iquery) ymeshline(iquery)];
 
-   elev = celev(idx(n));
-
-   % Tian - this is the key part where instead of xlower/ylower, we would
-   % need xriver/yriver where xriver/yriver are the cells along the channel
-   % that are lower than the starting point at the dam. we would actually
-   % need cx/cy/celev to be the x/y/elev of all points along the channel
-   % below the dam
-
-   ilower = celev<elev;
-   xlower = cx(ilower);
-   ylower = cy(ilower);
-   zlower = celev(ilower);
-
-   % sort x,y,z lower from high to low elevation
-   xyz      = sortrows([xlower ylower zlower],3,'descend');
-   xysearch = [xyz(:,1) xyz(:,2)];
-
-   % make the kdtree
-   kdtree   = createns(xysearch);
-
-   % find the indices of cells within rxy distance of each cell in xysearch
-   inearby = rangesearch(kdtree,xysearch,rxy);
-
-   for m = 1:numel(inearby)
-
-      icell = inearby{m};
-      xnearby = xysearch(icell,1);
-      ynearby = xysearch(icell,2);
-
-      % use this to see the result:
-      if plotfig == true
-         scatter(xnearby,ynearby,50,'g','filled');
-         pause(.2); 
-      end
-   end
-
-   % get all the cells that fall within rxy of any cell in xysearch (these
-   % are the cells that depend on this dam)
+   % query the mesh tree to find all cells below the dam that are also within
+   % rxy distance of the flowline. all cells in xyquery are below the dam, but
+   % the cells returned by the rangesearch could be above the dam, so we will
+   % trim those afterward.
+   inearby     = rangesearch(MeshTree,xyquery,rxy);
+   
+   % remove cells that are above the dam and concatenate the rest into one
+   % list. inearby is a cell array with one cell per flowline vertex. each cell
+   % has the indices of the Mesh that are within rxy distance of the mesh
+   % centroid that contains the flowline vertex (the flowline vertices are not
+   % exactly coincident with the cell centroids). but we want all the mesh
+   % cells that this dam contributes to, so flatten this into one list of all 
+   % mesh cells over all vertices, removing those above the dam along the way.
    allidx = [];
    for m = 1:numel(inearby)
-      allidx = [allidx; cell2mat(inearby(m))'];
+      idx = cell2mat(inearby(m))';
+      idx = idx(zmesh(idx)<elev);
+      allidx = [allidx;idx];
    end
-   allidx = unique(allidx);
+   
+   % save the unique values - these are the dependent cells for this dam
+   idepends{n} = unique(allidx);
 
-   % this should be all the dependent cells for each dam
-   idepends{n} = allidx;
-
-   % % this was gonna be a brute force search
-   % nlower = sum(ilower);   
-   % while nlower>0
-   %   nlower = nlower-1;
-   %   kdtree = createns(XY);
-   % end
-
+   
+   % see the result. only run on the first dam for demonstration (n=1)
+   if plotfig == true && n == 1
+      
+      scatter_hexmesh(MeshLine(imeshdams(n)),'FaceColor','y','MarkerSize',100);
+      scatter_hexmesh(MeshLine(iquery),'MarkerSize',20,'FaceColor','r');
+      
+      for m = 1:numel(inearby)
+         
+         icell = inearby{m};
+         patch_hexmesh(Mesh(icell),'FaceColor','g');
+         pause(.001); 
+         
+         % if using cartesian coords
+         % xnearby = xylower(icell,1);
+         % ynearby = xylower(icell,2);
+         % scatter(xnearby,ynearby,50,'g','filled');
+      end
+   end
+   
 end
 
 % add the dependency to the Dams table
 for n = 1:numdams
    Dams.DependentCells{n} = idepends{n};
 end
-
-
-% % testing kdtree
-% % use lat/lon
-% rLL         = 1.0;               % 1 degree?
-% LL          = [clon(:) clat(:)];
-% kdtreeLL    = createns(LL);
-% [iLL,dLL]   = rangesearch(kdtree,LL(1,:),rLL);
-% 
-% % use x/y
-% rXY         = 50*1000;           % 50 km? 
-% XY          = [cx(:) cy(:)];
-% kdtreeXY    = createns(XY);
-% [iXY,dXY]   = rangesearch(kdtree,XY(1,:),rXY);
 
 
 % % this confirms that the dLongitude_center/dLatitude_center is the
