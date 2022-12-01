e239363ed6f8ceca5717547fd5dc4fe95597caaf
mgcooper <mguycooper@gmail.com>
2022-12-01 15:10:29 -0800

    first commit of Config and Setup

---
+++ Config.m
@@ -0,0 +1,26 @@
+function Config()
+% CONFIG set user configuration, environment variables, etc.
+% 
+%  See also Setup
+
+% set environment variables to find data
+
+% path to ATS runoff data
+setenv('USER_ATS_DATA_PATH',[getenv('USERDATAPATH') 'interface/ATS']);
+
+% path to HILLSLOPER data files
+setenv('USER_HILLSLOPER_DATA_PATH',[getenv('USERDATAPATH') 'interface/hillsloper/trib_basin/newslopes'])
+
+% path to MOSART template files
+% setenv('USER_MOSART_TEMPLATE_PATH',[getenv('USERDATAPATH') 'interface/ATS/'])
+
+% need to organzie this
+setenv('USER_MOSART_TEMPLATE_PATH','/Users/coop558/work/data/e3sm/templates')
+
+% path to runoff files
+setenv('USER_MOSART_RUNOFF_PATH', '/Users/coop558/work/data/e3sm/forcing');
+
+% set path to domain/mosart config files
+setenv('USER_E3SM_CONFIG_PATH', '/Users/coop558/work/data/e3sm/config');
+
+% [keys,vals] = getuserpaths;
---
+++ Setup.m
@@ -0,0 +1,17 @@
+function Setup()
+% SETUP set paths etc.
+% 
+% See also Config
+
+% temporarily turn off warnings about paths not already being on the path
+% warning off
+
+% Get the path to this file, in case Setup is run from some other folder. More
+% robust than pwd(), but assumes the directory structure has not been modified.
+thispath = fileparts(mfilename('fullpath'));
+
+% add all paths then remove git paths
+addpath(genpath(thispath));
+rmpath(genpath([thispath filesep '.git*']));
+
+% add paths set in Config
