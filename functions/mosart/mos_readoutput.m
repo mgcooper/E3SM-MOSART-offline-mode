62add7f7878568e8f8123b06f6511e1dd4452f58
mgcooper <mguycooper@gmail.com>
2023-01-25 12:24:45 -0800

    fix mos_readoutput: try catch for monthly vs daily data, updated docu, general cleanup; added calls to plotatsmosart and atsmosarttable in the read_output script

--- functions/mosart/mos_readoutput.m
+++ functions/mosart/mos_readoutput.m
@@ -1,65 +1,96 @@
-function mosart = mos_readoutput(flist,varargin)
-%MOS_READOUTPUT reads the mosart .nc files in flist and sends back the outlet
+function mosart = mos_readoutput(pathdata,varargin)
+%MOS_READOUTPUT reads the mosart .nc files in pathdata and sends back the outlet
 %discharge in m3/s.
+%
+%  mosart = mos_readoutput(pathdata) returns the RIVER_DISCHARGE_OVER_LAND_LIQ
+%  data in the .nc mosart output files in pathdata, assumed to be annual files
+%
+%  mosart = mos_readoutput(pathdata,var) returns the data for variable var in
+%  the in the .nc mosart output files in pathdata
 % 
+%  mosart = mos_readoutput(___,'monthly') returns the data for variable var in
+%  the in the .nc mosart output files in pathdata
 % 
-% 
+% See also
 
-% this works if the data are daily, organized as annual files, 
+% this works if the data are daily, organized as annual files,
 % need to modify for monthly averages
-    
-% the second input must be the name of a variable 
-    if nargin > 1
-        var = varargin{1};
-    else
-        var = 'RIVER_DISCHARGE_OVER_LAND_LIQ';
-    end
-    
+
+% the second input must be the name of a variable
+var = 'RIVER_DISCHARGE_OVER_LAND_LIQ';
+if nargin == 2
+   var = varargin{1};
+elseif nargin == 3
+   var = varargin{1};
+   timestep = varargin{2};
+end
+
+flist = getlist(pathdata,'*.mosart.h0*');
+if isempty(flist)
+   flist = getlist(pathdata,'*.mosart.h1*');
+end
+
 % read the nc info, make a list of variables, get lat/lon
-    nfiles  = numel(flist);
-    fdir    = [flist(1).folder '/'];
-    info    = ncinfo([fdir flist(1).name]);
-    vars    = {info.Variables.Name};
-    lon     = double(ncread([fdir flist(1).name],'lon'));
-    lat     = double(ncread([fdir flist(1).name],'lat'));
+nfiles  = numel(flist);
+fdir    = flist(1).folder; % the run/ dir
+info    = ncinfo(fullfile(flist(1).folder,flist(1).name));
+vars    = {info.Variables.Name};
+lon     = double(ncread(fullfile(fdir,flist(1).name),'lon'));
+lat     = double(ncread(fullfile(fdir,flist(1).name),'lat'));
 
 % project the lat/lon to alaska albers
 %     load('proj_alaska_albers','proj_alaska_albers');
-   proj_alaska_albers = projcrs(3338,'Authority','EPSG');
-    [x,y]   = projfwd(proj_alaska_albers,lat,lon);
-    
+proj_alaska_albers = projcrs(3338,'Authority','EPSG');
+[x,y] = projfwd(proj_alaska_albers,lat,lon);
+
 % read all the data
-    for n = 1:nfiles
-        data(n) = ncreaddata([fdir flist(n).name],vars);
-    end
+for n = nfiles:-1:1
+   data(n) = ncreaddata(fullfile(fdir,flist(n).name),vars);
+end
 
 % stitch the discharge data into one long timeseries
 % init the discharge array (ncells x ndays x nyears = 3266 x 365 x 30)
-    [ndays,ncells] = size(data(1).(var));
-    
-    D       = nan(nfiles,ndays,ncells);
-    T       = nan(nfiles,ndays);
+[ndays,ncells] = size(data(1).(var));
 
-    % locate the outlet id
-    outID   = find(~isnan(data(1).RIVER_DISCHARGE_TO_OCEAN_LIQ(1,:)));
+D = nan(nfiles,ndays,ncells);
+T = nan(nfiles,ndays);
 
-    % mcdate is the actual calendar date, format is YYYYMMDD
-    
-    for n = 1:nfiles
-        
-        try
-            D(n,:,:) = data(n).(var);  % save RIVER_DISCHARGE_OVER_LAND_LIQ
-            T(n,:)   = data(n).mcdate; % 
-        catch
-        end
-        
-        % add the outlet
-        try
-            D(n,:,outID) = data(n).RIVER_DISCHARGE_TO_OCEAN_LIQ(:,outID);
-        catch
-        end 
-    end
-    
+% locate the outlet id
+try
+   outID = find(~isnan(data(1).RIVER_DISCHARGE_TO_OCEAN_LIQ(1,:)));
+catch ME
+   if strcmp(ME.message,'Unrecognized field name "RIVER_DISCHARGE_TO_OCEAN_LIQ".')
+      % this should mean that the mosart files are incl2 or higher and don't
+      % contain the RIVER_DISCHARGE_TO_OCEAN_LIQ variable because the
+      % frivinp_rtm file didn't request it. the outlet should be the index
+      % with all nan data, but the runoff data is missing in this case
+      try
+         outID = find(all(isnan(data(1).RIVER_DISCHARGE_OVER_LAND_LIQ),1));
+      catch ME
+         if strcmp(ME.message,'Unrecognized field name "RIVER_DISCHARGE_OVER_LAND_LIQ".')
+            rethrow(ME) 
+         end
+      end
+   end
+end
+
+% mcdate is the actual calendar date, format is YYYYMMDD
+
+for n = 1:nfiles
+
+   try
+      D(n,:,:) = data(n).(var);  % save RIVER_DISCHARGE_OVER_LAND_LIQ
+      T(n,:)   = data(n).mcdate; %
+   catch
+   end
+
+   % add the outlet
+   try
+      D(n,:,outID) = data(n).RIVER_DISCHARGE_TO_OCEAN_LIQ(:,outID);
+   catch
+   end
+end
+
 %     % use this to plot one file (should be one year of data)
 %     for n = 1:nfiles
 %       test = squeeze(D(n,:,:));
@@ -68,64 +99,106 @@ function mosart = mos_readoutput(flist,varargin)
 
 %    Discharge = readalldischarge(data,ndays,ncells);
 
-    
-    % check if any files have all nan data
-    allNan  = false(nfiles,1);
-    for n = 1:nfiles
-        if all(isnan(D(n,:,:)))
-            allNan(n)  = true;
-        end
-    end
-    
-    % warn the user if any files have all nan data
-    if any(allNan)
-       nanfiles = flist(allNan);
-       for n = 1:sum(allNan)
-         warning(['data all nan for file ' nanfiles(n).name])
-       end
-    end
-    
-    D       = D(~allNan,:,:);
-    T       = T(~allNan,:);
-    nfiles  = size(D,1);
-    
-    
-% compute mean annual D, std dev, and reshape into a timeseries
-    Davg    = squeeze(mean(D,1));
-    Dstd    = squeeze(std(D,[],1));
-    Dyrs    = D;
-    Tyrs    = datetime(T,'ConvertFrom','yyyymmdd');
-    
-% put it in a long timeseries
-    D       = permute(D,[2,1,3]); T = permute(T,[2,1]);
-    D       = reshape(D,ndays*nfiles,ncells);
-    T       = datetime(T(:),'ConvertFrom','yyyymmdd');
-    
-% shift the calendar back one day    
-    T = T - days(1);
-    
+
+% check if any files have all nan data
+allNan  = false(nfiles,1);
+for n = 1:nfiles
+   if all(isnan(D(n,:,:)))
+      allNan(n)  = true;
+   end
+end
+
+% warn the user if any files have all nan data
+if any(allNan)
+   nanfiles = flist(allNan);
+   for n = 1:sum(allNan)
+      warning(['data all nan for file ' nanfiles(n).name])
+   end
+end
+
+D       = D(~allNan,:,:);
+T       = T(~allNan,:);
+nfiles  = size(D,1);
+
+% if the data files are monthly, reshape to annual
+if size(D,2) == 30
+   nyears = size(D,1)*size(D,2)/365;
+   if mod(nyears,1) ~= 0
+      % the monthly data files truncate the data but I am not sure how. for the
+      % 1997-2003 runs the monthly files have 2550 values, but a no-leap
+      % calendar has 2555. Prob best to abandon monthly files anyway. UPDATE:
+      % the monthly files are written thirty days at a time, so each file isn't
+      % a calendar month. There are 7 months with 31 days then subtract two days
+      % for february and you get 5 days, which is the number missing but thats
+      % for one year, so still not sure. method below is a hacky fix but it
+      % appends 5 nan values at the end. 
+      D = permute(D,[2,1,3]);
+      D = reshape(D,[],size(D,3));
+      t1 = year(datetime(T(1),'ConvertFrom','yyyymmdd'));
+      t2 = year(datetime(T(end),'ConvertFrom','yyyymmdd'));
+      TT = transpose(datetime(t1,1,1):caldays(1):datetime(t2,12,31));
+      T0 = datetime(reshape(transpose(T),size(D,1),1),'ConvertFrom','yyyymmdd');
+      T0 = T0 - days(1);
+      DD = array2timetable(D ,'RowTimes',T0);
+      DD = retime(DD,TT,'fillwithmissing');
+      nyears = numel(unique(year(T0)));
+      ndays = 365;
+      D = reshape(table2array(rmleapinds(DD)),ndays,nyears,[]);
+
+      Tyrs = reshape(rmleapinds(TT),nyears,ndays);
+
+      Davg = squeeze(mean(D,2));
+      Dstd = squeeze(std(D,[],2));
+      Dyrs = D;
+
+      % put it in a long timeseries
+      D = reshape(D,ndays*nyears,ncells);
+      T = rmleapinds(TT);
+
+   end
+
+else
+
+   % D is numfiles x numdays x numcells. if data is annual this gets mean-annual
+   % compute mean annual D, std dev, and reshape into a timeseries
+   Tyrs    = datetime(T,'ConvertFrom','yyyymmdd') - days(1);
+   nyears  = numel(unique(year(Tyrs)));
+   Davg    = squeeze(mean(D,1));
+   Dstd    = squeeze(std(D,[],1));
+   Dyrs    = D;
+   
+   % put it in a long timeseries
+   D       = permute(D,[2,1,3]); T = permute(T,[2,1]);
+   D       = reshape(D,ndays*nyears,ncells);
+   T       = datetime(T(:),'ConvertFrom','yyyymmdd');
+   
+   % shift the calendar back one day
+   T = T - days(1);
+
+end
+
 % package output
-    mosart.data     = data;
-    mosart.D        = D;
-    mosart.T        = T;
-    mosart.Davg     = Davg;
-    mosart.Dstd     = Dstd;
-    mosart.Dyrs     = Dyrs;
-    mosart.Tyrs     = Tyrs;
-    mosart.info     = info;
-    mosart.lat      = lat;
-    mosart.lon      = lon;
-    mosart.x        = x;
-    mosart.y        = y;
-    mosart.outID    = outID;
-    
+mosart.data     = data;
+mosart.D        = D;
+mosart.T        = T;
+mosart.Davg     = Davg;
+mosart.Dstd     = Dstd;
+mosart.Dyrs     = Dyrs;
+mosart.Tyrs     = Tyrs;
+mosart.info     = info;
+mosart.lat      = lat;
+mosart.lon      = lon;
+mosart.x        = x;
+mosart.y        = y;
+mosart.outID    = outID;
+
 %     Qmodavg = mean(reshape(Dmod,365,nyrs),2);
 %     Tavg    = datenum(Tobs(1:365));
-        
-%     figure; plot(Davg(:,100)); hold on; 
+
+%     figure; plot(Davg(:,100)); hold on;
 %     plot(Davg(:,100)+(2.*Dstd(:,100)./sqrt(nfiles)));
 %     plot(Davg(:,100)-(2.*Dstd(:,100)./sqrt(nfiles)));
-%    
+%
 %     figure; plot(T,D(:,100)); hold on;
 %     for n = 1:nfiles
 %         plot(Tyrs(n,:),squeeze(Dyrs(n,:,100)),':','Color','r');
@@ -138,13 +211,13 @@ allvars  = fieldnames(testdata);
 isallnanorzero = true(numel(allvars),1);
 
 for n = 1:numel(allvars)
-   
+
    thisvar = allvars{n};
    thisdata = testdata.(thisvar);
-   
+
    % if the var is lowercase or all nan or all zero, continue
    if strcmp(thisvar,lower(thisvar)) || all(isnan(thisdata(:))) || all(thisdata(:)==0)
-      continue; 
+      continue;
    end
    % otherwise, set isallnanorzero false
    isallnanorzero(n) = false;
@@ -156,70 +229,70 @@ checkvars = allvars(~isallnanorzero);
 
 function Discharge = readalldischarge(data,ndays,ncells)
 
-   Dvars = { ...
-      'RIVER_DISCHARGE_OVER_LAND_ICE', ...
-      'RIVER_DISCHARGE_OVER_LAND_LIQ', ...
-      'DIRECT_DISCHARGE_TO_OCEAN_ICE', ...
-      'DIRECT_DISCHARGE_TO_OCEAN_LIQ', ...
-      'RIVER_DISCHARGE_TO_OCEAN_ICE', ...
-      'RIVER_DISCHARGE_TO_OCEAN_LIQ', ...
-      'TOTAL_DISCHARGE_TO_OCEAN_ICE', ...
-      'TOTAL_DISCHARGE_TO_OCEAN_LIQ'};
-   
-   nvars = numel(Dvars);
-   nfiles = numel(data);
-   
-   % this would read all the data
-   Dtemp = nan(ndays*nfiles,ncells,numel(Dvars));
-   for n = 1:nfiles
-      si = (n-1)*365+1;
-      ei = n*365;
-      thisdata = data(n);
-      for m = 1:nvars
-         thisvar = Dvars{m};
-         data_m = thisdata.(thisvar);
-         Dtemp(si:ei,:,m) = data_m;
-         % to get the sum over all cells:
-         % Dtemp(n,m,:) = nansum(data_m,2);
-      end
-   end
+Dvars = { ...
+   'RIVER_DISCHARGE_OVER_LAND_ICE', ...
+   'RIVER_DISCHARGE_OVER_LAND_LIQ', ...
+   'DIRECT_DISCHARGE_TO_OCEAN_ICE', ...
+   'DIRECT_DISCHARGE_TO_OCEAN_LIQ', ...
+   'RIVER_DISCHARGE_TO_OCEAN_ICE', ...
+   'RIVER_DISCHARGE_TO_OCEAN_LIQ', ...
+   'TOTAL_DISCHARGE_TO_OCEAN_ICE', ...
+   'TOTAL_DISCHARGE_TO_OCEAN_LIQ'};
 
-   % convert each tile to a table
-   TileSum = nan(ncells,nvars);
-   for n = 1:ncells
-      thistile = ['tile_' num2str(n)];
-      tiledata = squeeze(Dtemp(:,n,:));
-      Tiles.(thistile) = array2table(tiledata,'VariableNames',Dvars);
-      TileSum(n,:) = nansum(tiledata);
+nvars = numel(Dvars);
+nfiles = numel(data);
+
+% this would read all the data
+Dtemp = nan(ndays*nfiles,ncells,numel(Dvars));
+for n = 1:nfiles
+   si = (n-1)*365+1;
+   ei = n*365;
+   thisdata = data(n);
+   for m = 1:nvars
+      thisvar = Dvars{m};
+      data_m = thisdata.(thisvar);
+      Dtemp(si:ei,:,m) = data_m;
+      % to get the sum over all cells:
+      % Dtemp(n,m,:) = nansum(data_m,2);
    end
-   
-   TileSum = array2table(TileSum,'VariableNames',Dvars);
-   
+end
+
+% convert each tile to a table
+TileSum = nan(ncells,nvars);
+for n = 1:ncells
+   thistile = ['tile_' num2str(n)];
+   tiledata = squeeze(Dtemp(:,n,:));
+   Tiles.(thistile) = array2table(tiledata,'VariableNames',Dvars);
+   TileSum(n,:) = nansum(tiledata);
+end
+
+TileSum = array2table(TileSum,'VariableNames',Dvars);
+
 % % this would work but would just do it in the loop above
 %    % now convert each page to a table
 %    for n = 1:nvars
 %       thisvar = Dvars{m};
 %       Discharge.(thisvar) = array2table(Dtemp(:,:,n),'VariableNames',Dvars);
 %    end
-   
 
-   % this just sums the data as a check
-   Discharge = nan(nfiles,nvars);
-   for n = 1:nfiles
-      thisdata = data(n);
-      for m = 1:nvars
-         thisvar = Dvars{m};
-         data_m = thisdata.(thisvar);
-         Discharge(n,m) = nansum(data_m(:));
-         % to get the sum over all cells:
-         % Discharge(n,m,:) = nansum(data_m,2);
-      end
+
+% this just sums the data as a check
+Discharge = nan(nfiles,nvars);
+for n = 1:nfiles
+   thisdata = data(n);
+   for m = 1:nvars
+      thisvar = Dvars{m};
+      data_m = thisdata.(thisvar);
+      Discharge(n,m) = nansum(data_m(:));
+      % to get the sum over all cells:
+      % Discharge(n,m,:) = nansum(data_m,2);
    end
-   
-   Discharge = array2table(Discharge,'VariableNames',Dvars);
-   
+end
 
-   
+Discharge = array2table(Discharge,'VariableNames',Dvars);
+
+
+
 
 
 
@@ -227,28 +300,23 @@ function Discharge = readalldischarge(data,ndays,ncells)
 
 function tf = skipdata(thisdata)
 
-   % in matlab, a vector is a 2-d array that is either 1xN or Nx1
-   
-   tf = false;
-   
-   % this may not work b/c is* functions fail if the datatype is incompatible
-   while tf == false
-      
-      tf = isscalar(thisdata);
-      tf = isvector(thisdata);
-      tf = istable(thisdata);
-      tf = ischar(thisdata);
-      tf = isstring(thisdata);
-      
-   end
-   
-   %    if isscalar(thisdata) || istable(thisdata); continue; end
-   %    if skipdata(thisdata) == true; continue; end
+% in matlab, a vector is a 2-d array that is either 1xN or Nx1
 
+tf = false;
 
+% this may not work b/c is* functions fail if the datatype is incompatible
+while tf == false
 
+   tf = isscalar(thisdata);
+   tf = isvector(thisdata);
+   tf = istable(thisdata);
+   tf = ischar(thisdata);
+   tf = isstring(thisdata);
 
+end
 
+%    if isscalar(thisdata) || istable(thisdata); continue; end
+%    if skipdata(thisdata) == true; continue; end
 
 
 
--- scripts/mosart/postprocess/read_output_test_basin.m
+++ scripts/mosart/postprocess/read_output_test_basin.m
@@ -5,31 +5,28 @@ clean
 % output and map the runoff onto the hillslopes, locate the hillslope that
 % contains the basin outlet, and computes the runoff. 
 
-addpath(genpath('/Users/coop558/myprojects/matlab/bfra_dev_bk'));
+addpath(genpath('/Users/coop558/myprojects/matlab/bfra'));
 
 savedata = true;
 savefigs = false;
-sitename = 'trib_basin';
-run      = 'trib_basin.1997.2003.run.2022-11-22.ats';
-% run      = 'trib_basin.1997.2003.run.2022-11-18.ats';
-% run    = 'trib_basin.1998.2002.run.2022-07-21.ats';
+
+setenv('MOSART_SITENAME','trib_basin')
+setenv('MOSART_RUNID','trib_basin.1997.2003.run.2023-01-25-10-32.ats');
 
 %% set paths
 
-pathdata = [getenv('E3SMOUTPUTPATH') run '/run/'];
-pathsave = [getenv('E3SMOUTPUTPATH') run '/mat/'];
-
-if ~exist(pathsave,'dir'); mkdir(pathsave); addpath(pathsave); end; 
+pathdata = fullfile(getenv('E3SMOUTPUTPATH'),getenv('MOSART_RUNID'),'run');
+pathsave = fullfile(getenv('E3SMOUTPUTPATH'),getenv('MOSART_RUNID'),'mat');
 
 cd(pathdata)
 
 % load the sag river basin data
 load('/Users/coop558/work/data/interface/sag_basin/sag_data');
 
-sag.site_name = sitename;
+sag.site_name = getenv('MOSART_SITENAME');
 
 % for the trib basin, load that data and replace the data in 'sag'
-flow     = bfra_loadflow('SAGAVANIRKTOK R TRIB NR PUMP STA 3 AK');
+flow = bfra.loadflow('SAGAVANIRKTOK R TRIB NR PUMP STA 3 AK');
 sag.time = flow.Time;
 sag.flow = flow.Q;
 
@@ -39,15 +36,38 @@ sag.flow = flow.Q;
 
 % if h0 and h1 files exist, h1 = daily data saved annually, but depending
 % on how the tapes are set up the daily data can be h0 so gotta set this
-flist   = getlist(pathdata,'*.mosart.h0*');
-mosart  = mos_readoutput(flist);
+
+mosart  = mos_readoutput(pathdata);
 mosart  = mos_clipbasin(mosart,sag);
 
-
 %% save it
+
 if savedata == true
-    if ~exist(pathsave,'dir'); mkdir(pathsave); end
-    save([pathsave 'mosart.mat'],'mosart');
+   if ~exist(pathsave,'dir'); mkdir(pathsave); addpath(pathsave); end
+   
+   fname = fullfile(pathsave,'mosart.mat');
+   n = 1;
+   while isfile(fname)
+      n = n + 1;
+      fname = fullfile(pathsave,['mosart_' num2str(n) '.mat']);
+   end
+   save(fname,'mosart');
 end
 
+% convert to csv
+datatable = atsmosarttable(mosart);
+if savedata == true
+   fname = ['ats-mosart-' getenv('MOSART_RUNID') '.xlsx'];
+   fname = fullfile(getenv('MOSART_TESTBED'),fname);
+   writetimetable(datatable,fname);
+end
+
+%% plot it
+
+H = plotatsmosart;
+
+if savefigs == true
+   exportgraphics(f1,'ats-mosart-annual-avg.png','Resolution',300);
+   exportgraphics(f2,'ats-mosart-timeseries.png','Resolution',300);
+end
 
