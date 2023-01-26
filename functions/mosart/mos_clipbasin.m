2769831fd544b25c8656d302a37a65dd14aeca4d
mgcooper <mguycooper@gmail.com>
2023-01-25 16:28:11 -0800

    checked out Config and Startup and clipbasin from interface branch

--- functions/mosart/mos_clipbasin.m
+++ functions/mosart/mos_clipbasin.m
@@ -1,4 +1,4 @@
-function mosart = mos_clipbasin(mosart,sag)
+function mosart = mos_clipbasin(mosart,sag,varargin)
 
 % this function finds the hillslopes in the mosart data saved by
 % 'read_output_sag' that are contained within the gaged basin
@@ -8,14 +8,18 @@ Dmod     = mosart.D(:,mosart.outID);
 Tobs     = sag.time;
 Dobs     = sag.flow;
 
+% nov 2022 fixed the time shift problem on trib-basin and 1997 should be
+% spinup and 2003 should be duplicate of 2002 so clip to 1998/1/1 -
+% 2002/12/31
+
 % find the start and end of the overlapping period
 if strcmp(sag.site_name,'test_basin')
    t1       = datetime(2011,1,2);
    t2       = datetime(2017,1,1);
    Dtiles   = mosart.D;
 elseif strcmp(sag.site_name,'trib_basin')
-   t1       = datetime(1998,1,2);
-   t2       = datetime(2002,1,1);
+   t1       = datetime(1997,1,1);
+   t2       = datetime(2003,12,31);
    Dtiles   = mosart.D;
 else
    t1      = datetime(1983,1,1);
@@ -29,6 +33,8 @@ Tmod    = Tmod(isbetween(Tmod,t1,t2));
 Dobs    = Dobs(isbetween(Tobs,t1,t2));
 Tobs    = Tobs(isbetween(Tobs,t1,t2));
 
+% nov 2022 clipping Dtiles not sure why they weren't before
+Dtiles  = Dtiles(isbetween(Tmod,t1,t2),:);
 
 nyrs    = size(Dmod,1)/365;
 Qobsavg = mean(reshape(Dobs,365,nyrs),2);
