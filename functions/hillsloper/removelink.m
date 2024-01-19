97f4313a254267b52d48e5772f7b864b2e0e685d
mgcooper <mguycooper@gmail.com>
2024-01-19 14:38:20 -0800

    refactor(removelink): Rewrite using clear notation and detailed notes.
    Move node merging to mergenodes function.
    
    This was a major overhaul and improvement, not sure if actual errors were fixed.

--- functions/hillsloper/removelink.m
+++ functions/hillsloper/removelink.m
@@ -1,89 +1,220 @@
-function newlinks = removelink(newlinks,rm_link,us_link,ds_link,rp_link,slope_flag)
-%REMOVELINK Remove link from flow network and update connectivity attributes
-%
-%   rm_link     = id(s) to be removed
-%   us_links    = id(s) of links that flow into rm_link's upstream node
-%   ds_links    = id(s) of links that merge with rm_link's downstream node
-%   us_nodes    = rm_link's upstream node id
-%   ds_nodes    = rm_link's downstream node id
-%   rp_link     = id of link that replaces rm_link
-%   slope_flag  = remove the slope associated with rm_link
-%
-% NOTE: if i want to first run these functions, then re-run the
-% make_newlinks function, then I might need to return edits to the
-% nodes shapefile that fix the connections between those fields and the
-% ones here
-%
-% NOTE: the output of this function is such that the two us_links are
-% treated as a confluence with rp_link, whereas the one ds_link is
-% treated as a link that flows directly into rp_link. the fields are
-% consistent in this way, but the linkages between the fields in the
-% original 'nodes', 'links', and 'slopes' tables are broken (but could
-% be fixed, see detailed notes in archive)
-%
-% Note: previously mos_removelink
+function [links, nodes] = removelink(links, nodes, rm_link_ID, us_link_ID, ...
+      ds_link_ID, rp_link_ID, slope_flag)
+   %REMOVELINK Remove link from flow network and update connectivity attributes
+   %
+   %   rm_link     = id(s) to be removed
+   %   us_links    = id(s) of links that flow into rm_link's upstream node
+   %   ds_links    = id(s) of links that flow into rm_link's downstream node
+   %   us_nodes    = rm_link's upstream node id
+   %   ds_nodes    = rm_link's downstream node id
+   %   rp_link     = id of link that replaces rm_link
+   %   slope_flag  = remove the slope associated with rm_link
+   %
+   % In the example below, us_node = rm_node, and ds_node = rp_node.
+   % Before the merge, rp_link has two upstream links: ds_link and rm_link.
+   % After the merge, rp_link has three upstream links: ds_link, us_link1, and
+   % us_link2. There is one fewer node  and one fewer link in the network.
+   %
+   % Before:
+   %            us_node   rm_link   ds_node
+   %                  ↓      ↓      ↓
+   % o----------------o-------------o-------------o
+   %         ↑       /             /       ↑
+   %     us_links → /   ds_link → /     rp_link
+   %               /             /
+   %              o             /
+   %                           o
+   % After:
+   %                                ds_node
+   %                                ↓
+   % o-----------------------------o-------------o
+   %         ↑       -------------//       ↑
+   %     us_links → /             /     rp_link
+   %               /  ds_link →  /
+   %              o             /
+   %                           o
+   %
+   % The link removal creates a three-way confluence which does not exist in
+   % a standard hillsloper network.
+   %
+   % us_link fields and how they are modified:
+   %
+   % link_ID      :  not modified                                 *
+   % hs_ID        :  not modified                                 *
+   % us_node_ID   :  not modified                                 *
+   % us_link_ID   :  not modified                                 *
+   % us_hs_ID     :  not modified
+   % us_conn_ID   :  not modified
+   % ds_node_ID   :  us_node is replaced with ds_node             *
+   % ds_link_ID   :  rm_link is replaced with rp_link             *
+   % ds_hs_ID     :  set to the us_link and rp_link slope ID's
+   % ds_conn_ID   :  rm_link is replaced with rp_link
+   %
+   % ds_link fields and how they are modified:
+   %
+   % link_ID      :  not modified
+   % hs_ID        :  not modified
+   % us_node_ID   :  not modified
+   % us_link_ID   :  not modified
+   % us_hs_ID     :  not modified
+   % us_conn_ID   :  not modified
+   % ds_node_ID   :  not modified
+   % ds_link_ID   :  not modified
+   % ds_hs_ID     :  the rm_slope ID is removed
+   % ds_conn_ID   :  rm_link is removed. rp_link is already present.
+   %
+   % rp_link fields and how they are modified:
+   %
+   % link_ID      :  not modified
+   % hs_ID        :  not modified
+   % us_node_ID   :  not modified
+   % us_link_ID   :  not modified
+   % us_hs_ID     :  not modified
+   % us_conn_ID   :  not modified
+   % ds_node_ID   :  not modified
+   % ds_link_ID   :  not modified
+   % ds_hs_ID     :  the rm_slope ID is removed
+   % ds_conn_ID   :  rm_link is removed. rp_link is already present.
+   %
+   % rp_node fields that are modified:
+   %
+   % node_ID
+   % hs_ID
+   % conn
+   %
+   % See also:
 
-% get the indices of the link to be removed and its replacement
-idxrm = ismember([newlinks.link_ID], rm_link);
-idxrp = ismember([newlinks.link_ID], rp_link);
+   % TODO: add slopes and maybe basins input/output and convert to a general
+   % 'fixtopology' function ... for the bad topology in hillsloper v2, slopes
+   % does not have a bad slope, the hs_ID for the bad link is NaN. However,
+   % 'basins' does have it, but it cannot be identified from the fields in
+   % slopes, links, or nodes, because the hs_ID field for the bad link is NaN
+   % in links and nodes. I determined the hs_ID is 428 in basins manually.
 
-% get the us_link's and ds_link. although this works, to be careful, i
-% am keeping us_link and ds_link as inputs
-% idxi        = newlinks(idxrm).us_conn_ID~=rm_link;
-% us_link     = newlinks(idxrm).us_conn_ID(idxi);
-% idxi        = ~ismember(newlinks(idxrm).ds_conn_ID,[rm_link rp_link]);
-% ds_link     = newlinks(idxrm).ds_conn_ID(idxi);
-
-% get the id of the node to be removed and its replacement
-rm_node = newlinks(idxrm).us_node_ID;
-rp_node = newlinks(idxrp).us_node_ID;
-
-% get the id of the slope that drains to rm_link
-if slope_flag == true
-   rm_slope = newlinks(idxrm).hs_ID;
-end
-
-% replace the ds link/node/conn in the us_link's
-for n = 1:length(us_link)
-   idxn = ismember([newlinks.link_ID],us_link(n));
-   idxi = ismember([newlinks(idxn).ds_conn_ID],rm_link);
-
-   % make the replacements
-   newlinks(idxn).ds_link_ID = rp_link;
-   newlinks(idxn).ds_node_ID = rp_node;
-   newlinks(idxn).ds_conn_ID(idxi) = rp_link;
-end
-
-% deal with links that merge with rm_link at its downstream node
-for n = 1:length(ds_link)
-   idxn = ismember([newlinks.link_ID],ds_link(n));
-   idxi = ismember([newlinks(idxn).ds_conn_ID],rm_link);
-   if slope_flag == true
-      idxia = ismember([newlinks(idxn).ds_hs_ID],rm_slope);
-      idxib = ismember([newlinks(idxn).ds_hs_ID],-rm_slope);
-   else
-      idxia = [];
-      idxib = [];
+   if nargin < 7
+      slope_flag = false;
    end
-   newlinks(idxn).ds_hs_ID(idxia) = [];      % remove entirely
-   newlinks(idxn).ds_hs_ID(idxib) = [];      % remove entirely
-   newlinks(idxn).ds_conn_ID(idxi) = [];     % remove entirely
+   if nargin < 8
+      node_flag = false;
+   end
+
+   debug = false;
+
+   %% Prepare the information about the replacement
+
+   % Get the indices of the link to be removed and its replacement
+   i_rm_link = ismember([links.link_ID], rm_link_ID);
+   i_rp_link = ismember([links.link_ID], rp_link_ID);
+
+   % Get the id's of the slopes that rm_link and rp_link drain
+   if slope_flag == true
+      rm_slope_ID = links(i_rm_link).hs_ID;
+      rp_slope_ID = links(i_rp_link).hs_ID; % use this to update ds_hs_ID
+   end
+
+   % Get the id of the node to be removed and its replacement
+   rm_node_ID = links(i_rm_link).us_node_ID;
+   rp_node_ID = links(i_rp_link).us_node_ID;
+
+   %% Process nodes
+   [nodes, rm_node, rp_node] = mergenodes(nodes, rm_node_ID, rp_node_ID, ...
+      rm_slope_ID, rm_link_ID, true);
+
+   %% Process us_link's
+   rp_link = links(i_rp_link);
+   rm_link = links(i_rm_link);
+
+   % Replace the ds link/node/conn in the us_link's (the links upstream of the
+   % rm_link, this adjusts their downstream connectivity to connect to rp_link)
+   for n = 1:length(us_link_ID)
+
+      i_us_link = ismember([links.link_ID], us_link_ID(n));
+      this_link = links(i_us_link);
+
+      % Replace the downstream link and node ID
+      this_link.ds_link_ID = rp_link_ID;
+      this_link.ds_node_ID = rp_node_ID;
+
+      % This is used if the us/ds connectivity was built with findDownstreamLinks
+      if isfield(links, 'ds_conn_ID')
+         this_link.ds_conn_ID = rp_node.conn;
+      end
+      if slope_flag == true && isfield(this_link, 'ds_hs_ID')
+         this_link.ds_hs_ID = rp_node.hs_ID;
+      end
+
+      % Append rm_link to the us_links. This ensures the us links terminate at
+      % the rp_node and their path follows the hillslope discretization, but
+      % creates a co-located channel along the rm_link reach.
+      this_link.X = [rmnan(this_link.X) rm_link.X];
+      this_link.Y = [rmnan(this_link.Y) rm_link.Y];
+      if isfield(this_link, 'Lat')
+         this_link.Lat = [rmnan(this_link.Lat) rm_link.Lat];
+      end
+      if isfield(this_link, 'Lon')
+         this_link.Lon = [rmnan(this_link.Lon) rm_link.Lon];
+      end
+      % Compute the combined length and weighted-mean slope
+      combined_length = this_link.len_km + rm_link.len_km;
+      this_link.slope = wmean([this_link.slope rm_link.slope], ...
+         [this_link.len_km rm_link.len_km] ./ combined_length);
+      this_link.len_km = combined_length;
+
+      % Replace this us_link with the modified one
+      links(i_us_link) = this_link;
+   end
+
+   if debug == true
+      figure;
+      plot(this_link.X, this_link.Y, 'b'); hold on;
+      plot(rm_link.X, rm_link.Y, 'r');
+      scatter(rp_node.X, rp_node.Y, 'g', 'filled')
+      scatter(rm_node.X, rm_node.Y, 'r', 'filled')
+   end
+
+   %% Process ds_link: links that merge with rm_link at its downstream node
+   for n = 1:length(ds_link_ID)
+
+      i_ds_link = ismember([links.link_ID], ds_link_ID(n));
+      this_link = links(i_ds_link);
+
+      if isfield(this_link, 'ds_conn_ID')
+         this_link.ds_conn_ID = rp_node.conn;
+      end
+      if slope_flag == true && isfield(this_link, 'ds_hs_ID')
+         this_link.ds_hs_ID = rp_node.hs_ID;
+      end
+      % Replace the link
+      links(i_ds_link) = this_link;
+   end
+
+   %% Process rp_link
+   if isfield(rp_link, 'us_conn_ID')
+      rp_link.us_conn_ID = rp_node.conn;
+   end
+
+   % This should replace the method below
+   rp_link.us_link_ID = rp_node.conn;
+   rp_link.us_link_ID(ismember(rp_link.us_link_ID, rp_link_ID)) = [];
+
+   if isfield(rp_link, 'us_hs_ID')
+      rp_link.us_hs_ID = rp_node.hs_ID;
+   end
+
+   % Next shouldn't be necessary if rp_node was processed at the beginning.
+   % Remove rm_slope from rp_link's us_hs_ID
+   if slope_flag == true && isfield(rp_link, 'us_hs_ID')
+      idx_p = ismember(rp_link.us_hs_ID, rm_slope_ID);
+      idx_m = ismember(rp_link.us_hs_ID, -rm_slope_ID);
+      rp_link.us_hs_ID(idx_p) = [];
+      rp_link.us_hs_ID(idx_m) = [];
+   end
+
+   %% Final steps
+
+   % Replace the rp_link with the modified version
+   links(i_rp_link) = rp_link;
+
+   % Remove the link
+   links(i_rm_link) = [];
 end
-
-% remove rm_link from the us_conn_ID field
-idxi = ismember([newlinks(idxrp).us_conn_ID],rm_link);
-newlinks(idxrp).us_conn_ID(idxi) = [];
-
-% remove rm_slope from rp_link's us_hs_ID
-if slope_flag == true
-   idx_p = ismember(newlinks(idxrp).us_hs_ID,rm_slope);
-   idx_m = ismember(newlinks(idxrp).us_hs_ID,-rm_slope);
-   newlinks(idxrp).us_hs_ID(idx_p) = [];
-   newlinks(idxrp).us_hs_ID(idx_m) = [];
-end
-
-% remove the orphan link
-newlinks(idxrm) = [];
-% note that this changes the indices, so if anything were to follow
-% this, they would need to be updated
-
--- functions/hillsloper/removeslopes.m
+++ functions/hillsloper/removeslopes.m
@@ -1,65 +1,63 @@
 function newslopes = removeslopes(slopes,rm_slope,rp_slope,rp_flag)
-%REMOVESLOPES merge one hillslope with another
-% 
-% slopes    = structure (shapefile) containing hillslopes
-% rm_slope  = id of the slope to be removed
-% rp_slope  = id of the slope that replaces (merges with) rm_slope
-% rp_flag   = 1 for rp_slope plus, 0 for rp_slope minus
+   %REMOVESLOPES merge one hillslope with another
+   %
+   % slopes    = structure (shapefile) containing hillslopes
+   % rm_slope  = id of the slope to be removed
+   % rp_slope  = id of the slope that replaces (merges with) rm_slope
+   % rp_flag   = 1 for rp_slope plus, 0 for rp_slope minus
 
-% indices of the hillslopes
-idxrm_p = find([slopes.hs_ID] == rm_slope);     % 'plus' = larger
-idxrm_m = find([slopes.hs_ID] == -rm_slope);    % 'minus' = smaller
-if rp_flag == 1
-   idrp  = rp_slope; % keep track for easier indexing at the end
-   idxrp = find([slopes.hs_ID] == rp_slope);
-else
-   idrp  = -rp_slope;
-   idxrp = find([slopes.hs_ID] == -rp_slope);
+   % indices of the hillslopes
+   idxrm_p = find([slopes.hs_ID] == rm_slope);     % 'plus' = larger
+   idxrm_m = find([slopes.hs_ID] == -rm_slope);    % 'minus' = smaller
+   if rp_flag == 1
+      idrp  = rp_slope; % keep track for easier indexing at the end
+      idxrp = find([slopes.hs_ID] == rp_slope);
+   else
+      idrp  = -rp_slope;
+      idxrp = find([slopes.hs_ID] == -rp_slope);
+   end
+
+   % pull out the hillslopes
+   hsrm_p = slopes(idxrm_p);  % hillslope remove plus
+   hsrm_m = slopes(idxrm_m);  % hillslope remove minus
+   hsrp = slopes(idxrp);      % hillslope replace (here, plus)
+
+   % create a polyshape to extract the new boundaries
+   xnew = [hsrp.X hsrm_m.X hsrm_p.X];
+   ynew = [hsrp.Y hsrm_m.Y hsrm_p.Y];
+   hspoly = polyshape(xnew,ynew);         % merged polyshape
+   [xnew,ynew] = boundary(hspoly);        % just the boundary
+   hspoly = polyshape(xnew,ynew);         % make a new polyshape
+   [xlim,ylim] = boundingbox(hspoly);     % new boundingbox
+   % if the weird stuff near the outlet is problematic, try reverting to using
+   % xnew,ynew as the x/y coordinates of the merged hillslope
+
+   % new hillslope, with combined x/y/etc.
+   nhs             = hsrp;   % inherit fields
+   nhs.BoundingBox = [xlim' ylim'];
+   nhs.X           = xnew;
+   nhs.Y           = ynew;
+   nhs.ncells      = hsrp.ncells+hsrm_m.ncells+hsrm_p.ncells;
+   nhs.da          = hsrp.da+hsrm_m.da+hsrm_p.da;
+   nhs.harea       = hsrp.harea+hsrm_m.harea+hsrm_p.harea;
+
+   % area-weighted elevation and slope
+   helev1          = hsrp.helev*hsrp.harea/nhs.harea;
+   helev2          = hsrm_m.helev*hsrm_m.harea/nhs.harea;
+   helev3          = hsrm_p.helev*hsrm_p.harea/nhs.harea;
+   nhs.helev       = roundn(helev1+helev2+helev3,0);
+
+   hslp1           = hsrp.hslp*hsrp.harea/nhs.harea;
+   hslp2           = hsrm_m.hslp*hsrm_m.harea/nhs.harea;
+   hslp3           = hsrm_p.hslp*hsrm_p.harea/nhs.harea;
+   nhs.hslp        = roundn(hslp1+hslp2+hslp3,-4);
+
+   % remove and replace (update idx after each removal)
+   newslopes       = slopes;
+   idx             = find([newslopes.hs_id] == rm_slope);
+   newslopes(idx)  = [];
+   idx             = find([newslopes.hs_id] == -rm_slope);
+   newslopes(idx)  = [];
+   idx             = find([newslopes.hs_id] == idrp);
+   newslopes(idx)  = nhs;
 end
-
-% pull out the hillslopes
-hsrm_p = slopes(idxrm_p);  % hillslope remove plus
-hsrm_m = slopes(idxrm_m);  % hillslope remove minus
-hsrp = slopes(idxrp);      % hillslope replace (here, plus)
-
-% create a polyshape to extract the new boundaries
-xnew = [hsrp.X hsrm_m.X hsrm_p.X];
-ynew = [hsrp.Y hsrm_m.Y hsrm_p.Y];
-hspoly = polyshape(xnew,ynew);         % merged polyshape
-[xnew,ynew] = boundary(hspoly);        % just the boundary
-hspoly = polyshape(xnew,ynew);         % make a new polyshape
-[xlim,ylim] = boundingbox(hspoly);     % new boundingbox
-% if the weird stuff near the outlet is problematic, try reverting to using
-% xnew,ynew as the x/y coordinates of the merged hillslope
-
-% new hillslope, with combined x/y/etc.
-nhs             = hsrp;   % inherit fields
-nhs.BoundingBox = [xlim' ylim'];
-nhs.X           = xnew;
-nhs.Y           = ynew;
-nhs.ncells      = hsrp.ncells+hsrm_m.ncells+hsrm_p.ncells;
-nhs.da          = hsrp.da+hsrm_m.da+hsrm_p.da;
-nhs.harea       = hsrp.harea+hsrm_m.harea+hsrm_p.harea;
-
-% area-weighted elevation and slope
-helev1          = hsrp.helev*hsrp.harea/nhs.harea;
-helev2          = hsrm_m.helev*hsrm_m.harea/nhs.harea;
-helev3          = hsrm_p.helev*hsrm_p.harea/nhs.harea;
-nhs.helev       = roundn(helev1+helev2+helev3,0);
-
-hslp1           = hsrp.hslp*hsrp.harea/nhs.harea;
-hslp2           = hsrm_m.hslp*hsrm_m.harea/nhs.harea;
-hslp3           = hsrm_p.hslp*hsrm_p.harea/nhs.harea;
-nhs.hslp        = roundn(hslp1+hslp2+hslp3,-4);
-
-% remove and replace (update idx after each removal)
-newslopes       = slopes;
-idx             = find([newslopes.hs_id] == rm_slope);
-newslopes(idx)  = [];
-idx             = find([newslopes.hs_id] == -rm_slope);
-newslopes(idx)  = [];
-idx             = find([newslopes.hs_id] == idrp);
-newslopes(idx)  = nhs;
-
-end
-
